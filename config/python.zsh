b8eebe1dfca5d16889404fb7381daf689c19b04a
Robb Romans <robb.romans@rackspace.com>
2021-05-04 16:14:49 -0500

    Set aliases for Brew Python

--- config/python.zsh
+++ config/python.zsh
@@ -1,10 +1,16 @@
-if (( $+commands[pyenv] )); then
-  eval "$(pyenv init -)"
-fi
+# if (( $+commands[pyenv] )); then
+#   eval "$(pyenv init -)"
+# fi
 
-if (( $+commands[pyenv-virtualenv-init] )); then
-  eval "$(pyenv virtualenv-init -)"
-fi
+# if (( $+commands[pyenv-virtualenv-init] )); then
+#   eval "$(pyenv virtualenv-init -)"
+# fi
 
-# Disable python virtualenv environment prompt prefix
-export VIRTUAL_ENV_DISABLE_PROMPT=true
+# # Disable python virtualenv environment prompt prefix
+# export VIRTUAL_ENV_DISABLE_PROMPT=true
+
+# Use Brew Python
+if (( $+commands[brew] )); then
+  alias python=$(brew --prefix)/bin/python3
+  alias pip=$(brew --prefix)/bin/pip3
+fi
