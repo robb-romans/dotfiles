406c96e2eb70ef84005275faed57d04f22a207b3
Robb Romans <rromans@squarespace.com>
2022-11-11 19:30:15 -0600

    Fix zsh brew python path

--- config/python.zsh
+++ config/python.zsh
@@ -1,19 +1,20 @@
-# if (( $+commands[pyenv] )); then
-#   export PYENV_ROOT="$HOME/.pyenv"
-#   export PATH="$PYENV_ROOT/bin:$PATH
-#   eval "$(pyenv init --path)"
-#   eval "$(pyenv init -)"
-# fi
+if (( $+commands[pyenv] )); then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+fi
 
-# if (( $+commands[pyenv-virtualenv-init] )); then
-#   eval "$(pyenv virtualenv-init -)"
-# fi
+if (( $+commands[pyenv-virtualenv-init] )); then
+  eval "$(pyenv virtualenv-init -)"
+fi
 
-# # Disable python virtualenv environment prompt prefix
-# export VIRTUAL_ENV_DISABLE_PROMPT=true
+# Disable python virtualenv environment prompt prefix
+# https://stackoverflow.com/questions/10406926/how-do-i-change-the-default-virtualenv-prompt
+export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # Use Brew Python
 if (( $+commands[brew] )); then
-  alias python=$(brew --prefix)/bin/python3
-  alias pip=$(brew --prefix)/bin/pip3
+  alias python=$(brew --prefix)/opt/python/libexec/bin/python
+  alias pip=$(brew --prefix)/opt/python/libexec/bin/pip
 fi
