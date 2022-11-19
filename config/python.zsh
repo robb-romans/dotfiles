0d7240c742ed44cbb310547b575cb69428e8c487
Robb Romans <rromans@squarespace.com>
2022-11-18 19:49:10 -0600

    Set Brew Python path instead of alias
    Use more ZSHelly syntax in init file
    Make more idempotent with typeset

--- config/python.zsh
+++ config/python.zsh
@@ -13,8 +13,15 @@ fi
 # https://stackoverflow.com/questions/10406926/how-do-i-change-the-default-virtualenv-prompt
 export VIRTUAL_ENV_DISABLE_PROMPT=true
 
+# # Use Brew Python
+# if (( $+commands[brew] )); then
+#   alias python=$(brew --prefix)/opt/python/libexec/bin/python
+#   alias pip=$(brew --prefix)/opt/python/libexec/bin/pip
+# fi
+
 # Use Brew Python
+# Set path instead of alias
 if (( $+commands[brew] )); then
-  alias python=$(brew --prefix)/opt/python/libexec/bin/python
-  alias pip=$(brew --prefix)/opt/python/libexec/bin/pip
+   typeset -U path PATH
+   path=($(brew --prefix)/opt/python/libexec/bin "$path[@]")
 fi
