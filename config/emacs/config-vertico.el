a933f0b194f27947684a0590d4c7d129ae2d1601
Robb Romans <rromans@squarespace.com>
2023-03-11 19:04:49 -0600

    vertico

---
+++ config/emacs/config-vertico.el
@@ -0,0 +1,12 @@
+;; Vertico
+;; https://github.com/minad/vertico
+
+(use-package vertico
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode))
